79eb197c41d2384994693e5af3eee44f56441e13
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-03-05 17:50:19 +0100

    Add logout route

--- src/routes/Logout.jsx
+++ src/routes/Logout.jsx
@@ -2,9 +2,11 @@ import { useEffect, useContext } from "react";
 import { Context } from "./utils/globalStateContext.js";
 import routes from "../routes.jsx";
 import requests from "../utils/requests.js";
+import ws from "../websocket/ws.js";
 
 function Logout() {
-    const { setIsLogged, setPublicUsername, setPrivateUsername } = useContext(Context);
+    const { setIsLogged, setPublicUsername, setPrivateUsername, setPrivateKey } =
+        useContext(Context);
     useEffect(() => {
         const controller = new AbortController();
         (async () => {
@@ -13,9 +15,12 @@ function Logout() {
                 if (response.error) {
                     console.log(response.error);
                 }
+                // we should also close the ws connection!
                 setIsLogged(false);
+                ws.closeSocket();
                 setPublicUsername(null);
                 setPrivateUsername(null);
+                setPrivateKey(null);
                 routes.navigate("/");
                 return;
             } catch (err) {

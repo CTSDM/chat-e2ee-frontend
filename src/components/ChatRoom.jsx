84b6c810da7c8ab84b8868f0b5b979344da2d5de
CTSDM <156813428+CTSDM@users.noreply.github.com>
2025-06-03 10:58:21 +0200

    Add scroll bar for the one to one chat

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 import TextArea from "./TextArea.jsx";
 import { env } from "../../config/config.js";
 import MessageBubble from "./MessageBubble.jsx";
-import styles from "./ChatRoom.module.css";
 import { useEffect, useRef } from "react";
 import { chatUtils } from "../utils/utils.js";
+import styles from "./ChatRoom.module.css";
 
 function ChatRoom({ messages, handleOnSubmit, handleOnRender, username, target }) {
     const input = env.inputs.message;
@@ -12,6 +12,12 @@ function ChatRoom({ messages, handleOnSubmit, handleOnRender, username, target }
     const refForm = useRef(null);
     const messagesArr = chatUtils.getCurrentMessages(messages, target.toLowerCase());
     const refScrollbar = useRef(null);
+    const refMessagesContainer = useRef(null);
+    const refSubcontainer = useRef(null);
+    const refAuxMsgContainer = useRef({
+        active: false,
+        initialPosition: { scrollTop: null, clickY: null },
+    });
 
     useEffect(() => {
         if (messagesArr) {
@@ -23,6 +29,34 @@ function ChatRoom({ messages, handleOnSubmit, handleOnRender, username, target }
         }
         // we focus the input
     }, [handleOnRender, messagesArr]);
+
+    useEffect(() => {
+        window.addEventListener("mouseup", disableState);
+        window.addEventListener("mousemove", updateMsgContainerPosition);
+        function disableState() {
+            refAuxMsgContainer.current.active = false;
+        }
+        function updateMsgContainerPosition(e) {
+            if (refAuxMsgContainer.current.active === true) {
+                // we calculate the new scrollTop according to the saved coordinates
+                // we have to translate these coordinates to the dimensions of the scrollbar
+                // or something similar I think like
+                const diff = (e.clientY - refAuxMsgContainer.current.initialPosition.clickY) * 3;
+                const newScroll = refAuxMsgContainer.current.initialPosition.scrollTop + diff;
+                const maxScroll =
+                    refMessagesContainer.current.scrollHeight -
+                    refMessagesContainer.current.clientHeight;
+                refMessagesContainer.current.scrollTop = Math.min(
+                    Math.max(0, newScroll),
+                    maxScroll,
+                );
+            }
+        }
+        return () => {
+            window.removeEventListener("mouseup", disableState);
+            window.removeEventListener("mousemove", updateMsgContainerPosition);
+        };
+    }, []);
 
     useEffect(() => {
         if (refForm.current) {
@@ -46,18 +80,52 @@ function ChatRoom({ messages, handleOnSubmit, handleOnRender, username, target }
     }
 
     function onMouseOver() {
-        refScrollbar.current.style["background-color"] = "blue";
+        // we obtain the height of the message container
+        const [newHeight, bottomMargin] = getSizePositionScrollbar(
+            refSubcontainer.current,
+            refMessagesContainer.current,
+        );
+        if (newHeight === 0) {
+            refScrollbar.current.style["opacity"] = "0";
+            refScrollbar.current.style["margin-bottom"] = "0px";
+            refScrollbar.current.classList.remove(`${styles.active}`);
+        } else {
+            // we get the size and where we should place the scroll bar;
+            refScrollbar.current.style["min-height"] = `${newHeight}px`;
+            refScrollbar.current.style["opacity"] = "1";
+            refScrollbar.current.style["bottom"] = `${bottomMargin}%`;
+            refScrollbar.current.classList.add(`${styles.active}`);
+        }
     }
 
     function onMouseOut() {
-        refScrollbar.current.style["background-color"] = "";
+        refScrollbar.current.style["opacity"] = "0";
+    }
+
+    function onMouseDown(e) {
+        // only works for left click
+        if (e.button === 0) {
+            refAuxMsgContainer.current.initialPosition.scrollTop =
+                refMessagesContainer.current.scrollTop;
+            refAuxMsgContainer.current.initialPosition.clickY = e.clientY;
+            refAuxMsgContainer.current.active = true;
+        }
     }
 
     return (
         <div className={styles.container}>
             <div className={styles.contact}>{target}</div>
-            <div className={styles.subContainer} onMouseOver={onMouseOver} onMouseOut={onMouseOut}>
-                <div className={styles.messagesContainer}>
+            <div
+                className={styles.subContainer}
+                onMouseOver={onMouseOver}
+                onMouseOut={onMouseOut}
+                ref={refSubcontainer}
+            >
+                <div
+                    className={styles.messagesContainer}
+                    ref={refMessagesContainer}
+                    onScroll={onMouseOver}
+                >
                     {messagesArr.map((message) => (
                         <MessageBubble
                             key={message.id}
@@ -71,7 +139,11 @@ function ChatRoom({ messages, handleOnSubmit, handleOnRender, username, target }
                         />
                     ))}
                 </div>
-                <div className={styles.scrollbar} ref={refScrollbar}></div>
+                <div
+                    className={styles.scrollbar}
+                    ref={refScrollbar}
+                    onMouseDown={onMouseDown}
+                ></div>
             </div>
             <div className={styles.form}>
                 <TextArea
@@ -84,6 +156,28 @@ function ChatRoom({ messages, handleOnSubmit, handleOnRender, username, target }
             </div>
         </div>
     );
+}
+
+function getSizePositionScrollbar(container, messageContainer) {
+    const messagesContainerHeight = messageContainer.scrollHeight;
+    const visibleHeight = container.clientHeight;
+    const ratio = visibleHeight / messagesContainerHeight;
+    let newHeight, offsetBottom; // ofsetBottom in percentage
+    if (ratio >= 1) {
+        newHeight = 0;
+        offsetBottom = 0;
+    } else {
+        newHeight = ratio * visibleHeight;
+        const verticalOffset = messageContainer.scrollTop;
+        const ratio2 =
+            (messagesContainerHeight - verticalOffset - visibleHeight) / messagesContainerHeight;
+        if (ratio2 >= 1) offsetBottom = 100;
+        else {
+            // offsetBottom = 100 * (1 - ratio2);
+            offsetBottom = ratio2 * 100;
+        }
+    }
+    return [newHeight, offsetBottom];
 }
 
 ChatRoom.propTypes = {
